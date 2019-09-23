defc924d8041b8a19ddf2d95e3c640245e3c4704
Augusto Goncalves <augusto.goncalves@autodesk.com>
2019-09-23 14:01:36 -0700

    setup

--- forgeSample/wwwroot/js/main.js
+++ forgeSample/wwwroot/js/main.js
@@ -1,16 +1,46 @@
 function getAccounts(callback) {
+    isAuthenticated(function () {
+        jQuery.ajax({
+            url: '/api/forge/datamanagement/hubs',
+            success: function (hubs) {
+                //$('.selectpicker').selectpicker({title: 'Select BIM 360 Account'}).selectpicker('render');
+                hubs.forEach(function (hub) {
+                    var o = new Option(hub.name, hub.id);
+                    /// jquerify the DOM object 'o' so we can use the html method
+                    $(o).html(hub.name);
+                    $("#bim360accounts").append(o);
+                })
+                $('.selectpicker').selectpicker('refresh');
+                if (callback) callback(hubs);
+            }
+        });
+    });
+}
+
+function isAuthenticated(callback) {
     jQuery.ajax({
-        url: '/api/forge/datamanagement/hubs',
-        success: function (hubs) {
-            //$('.selectpicker').selectpicker({title: 'Select BIM 360 Account'}).selectpicker('render');
-            hubs.forEach(function (hub) {
-                var o = new Option(hub.name, hub.id);
-                /// jquerify the DOM object 'o' so we can use the html method
-                $(o).html(hub.name);
-                $("#bim360accounts").append(o);
+        url: '/api/forge/oauth/token',
+        success: function (res) {
+            // yes, it is signed in...
+            $('#signOut').show();
+
+            // prepare sign out
+            $('#signOut').click(function () {
+                $('#hiddenFrame').on('load', function (event) {
+                    location.href = '/api/forge/oauth/signout';
+                });
+                $('#hiddenFrame').attr('src', 'https://accounts.autodesk.com/Authentication/LogOut');
+                // learn more about this signout iframe at
+                // https://forge.autodesk.com/blog/log-out-forge
             })
-            $('.selectpicker').selectpicker('refresh');
-            if (callback) callback(hubs);
+
+            // finally:
+            //showUser();
+
+            if (callback) callback();
+        },
+        error: function () {
+            $('#signInModal').modal('toggle');
         }
     });
 }
@@ -19,4 +49,17 @@ $(document).ready(function () {
     $('#doSearch').click(function () {
         location.href = '/search?q=' + $('#searchString').val() + '&accountId=' + $('#bim360accounts').val()
     });
+
+    $('#indexAccount').click(function () {
+        location.href = '/setup';
+    });
+
+    $('#autodeskSigninButton').click(function () {
+        jQuery.ajax({
+            url: '/api/forge/oauth/url',
+            success: function (url) {
+                location.href = url;
+            }
+        });
+    })
 });
